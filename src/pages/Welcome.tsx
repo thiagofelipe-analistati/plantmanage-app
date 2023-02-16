4d61672eeb1f99170ceb0a375e29ece68cd04bf1
Thiago Felipe <thiagofelipe_analistati@outlook.com>
2023-02-16 10:28:57 -0300

    Page Welcome ok

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -8,15 +8,18 @@ export  function Welcome(){
  
     return(
         <SafeAreaView style={styles.contanaier}>
-            <Text style={styles.title}> Gerencie {'\n'}suas plantas {'\n'}de forma fácil</Text>
-            <Image source={wateringImg} style={styles.image} resizeMode="contain" />
-            <Text style={styles.subtitle}> Não esqueça mais de regar suas plantas, Nós cuidamos de lembrar voce sempre que precisar.</Text>
-            <TouchableOpacity 
-                style={styles.button} 
-                activeOpacity={0.7}
-            >
-                <Feather  style={styles.buttontext} name="chevron-right" />
-            </TouchableOpacity>
+            <View style={styles.wrapper}> 
+                <Text style={styles.title}> Gerencie {'\n'}suas plantas de {'\n'} forma fácil</Text>
+                <Image source={wateringImg} style={styles.image} resizeMode="contain" />
+                <Text style={styles.subtitle}> Não esqueça mais de regar suas {'\n'} plantas,
+                 Nós cuidamos de lembrar você {'\n'} sempre que precisar.</Text>
+                <TouchableOpacity 
+                    style={styles.button} 
+                    activeOpacity={0.7}
+                >
+                    <Feather  style={styles.buttontext} name="chevron-right" />
+                </TouchableOpacity>
+            </View>
         </SafeAreaView>
     )
 }
@@ -28,18 +31,17 @@ const styles = StyleSheet.create({
         justifyContent:'space-around'
     },
     title:{
-        fontSize: 32,
+        fontSize: 28,
         fontWeight: 'bold',
         textAlign: 'center',
         color: colors.heading,
-        marginTop: 38
+        marginTop: 34
     },
     subtitle: {
         textAlign: 'center',
         fontSize: 18,
         color: colors.heading,
         paddingHorizontal: 20
-
     },
     image: {
         height: Dimensions.get('window').width*0.7
@@ -56,5 +58,11 @@ const styles = StyleSheet.create({
     buttontext: {
         color: colors.white,
         fontSize: 24,
+    },
+    wrapper:{
+        flex: 1,
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        paddingHorizontal: 20
     }
 })
